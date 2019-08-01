cb4204db5b50fb6ed1f42fda5eadd7ee9da95510
TKerber <tuco.kerber@gmail.com>
2019-08-01 20:56:48 -0300

    primeiro commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exemplo de uso de git hub!");
+            Console.WriteLine("teste!");
+            Console.ReadKey();
+        }
+    }
+}
