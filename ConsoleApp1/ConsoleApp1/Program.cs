4b7f243871ec84e57be25feacc633dd6118d2e30
TKerber <tuco.kerber@gmail.com>
2019-08-08 15:03:35 -0300

    Mudança no metodo escreveNaTela, agora recebendo parametros

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,13 +4,15 @@ namespace ConsoleApp1
 {
     class Program
     {
-        static void escreveNaTela()
+        
+        static void escreveNaTela(String txt)
         {
-            Console.WriteLine("vamos fazer um terceiro commit!");
+            Console.WriteLine(txt);
         }
         static void Main(string[] args)
         {
-            escreveNaTela();
+            String txt = "Alo gitHub";
+            escreveNaTela(txt);
             Console.ReadKey();
         }
     }
