30db9a383488969968f028636c2da278eb4ba5c6
TKerber <tuco.kerber@gmail.com>
2019-08-08 15:47:31 -0300

    Funçao exclusiva da versao beta

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,9 +8,14 @@ namespace ConsoleApp1
         {
             Console.WriteLine("vamos fazer um terceiro commit!");
         }
+        static void escreveNaTela3(String txt)
+        {
+            Console.WriteLine(txt);
+        }
         static void Main(string[] args)
         {
             escreveNaTela();
+            escreveNaTela3("versao beta!");
             Console.ReadKey();
         }
     }
